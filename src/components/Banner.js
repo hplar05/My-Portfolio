b0242f560ff8f95f0b08f80b759d7aa1ead15a53
Hp Lar <Ralph.saladino05@gmail.com>
2023-05-27 06:13:11 +0800

    commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import Image from '../assets/avatar.svg'
-import {FaGithub, FaFacebook, FaYoutube} from 'react-icons/fa'
+import Image from '../assets/avatar.png';
+import { FaGithub, FaFacebook, FaYoutube } from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
-import {motion} from 'framer-motion'
-import {fadeIn} from '../variants'
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+
 const Banner = () => {
-  return <div className='section' id='home'>
-    Banner
-  </div>;
+  return (
+    <section className='section' id='home'>
+      <div className="container mx-auto">
+        <div>
+          {/* Your text */}
+          <div></div>
+          {/* Modified image */}
+          <div className="">
+            <img
+              src={Image}
+              alt=''
+              className="w-80 h-80 rounded-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
