251d0797c6e49c045f217fa72f6c24aafa946c24
Hp Lar <Ralph.saladino05@gmail.com>
2023-05-27 05:34:19 +0800

    commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import Image from '../assets/avatar.svg'
+import {FaGithub, FaFacebook, FaYoutube} from 'react-icons/fa'
+import { TypeAnimation } from 'react-type-animation';
+import {motion} from 'framer-motion'
+import {fadeIn} from '../variants'
 const Banner = () => {
-  return <div className='section' id='home'>Banner</div>;
+  return <div className='section' id='home'>
+    Banner
+  </div>;
 };
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,11 @@ const Header = () => {
   return (
     <header className="py-8">
       <div className="container mx-auto">
-        <div className="flex justify-start">
+        <div className="flex justify-between items-center ">
           <a href="#">
             <img src={Logo} alt="Logo" className="w-28" />
           </a>
+          <button className='btn btn-sm cursor-pointer'>More about me</button>
         </div>
       </div>
     </header>
