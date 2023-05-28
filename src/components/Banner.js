e90b7f381a7cc630e4c244b673d7da7e16e2ed1a
Hp Lar <Ralph.saladino05@gmail.com>
2023-05-29 05:05:15 +0800

    commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,15 +7,31 @@ import { fadeIn } from '../variants';
 
 const Banner = () => {
   return (
-    <section className='min-h-[100vh] lg:min-h-[78vh] flex items-center' id='home'>
+    <section className='min-h-[78vh] lg:min-h-[78vh] flex items-center' id='home'>
       <div className="container mx-auto">
         <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
           {/* Your text */}
           <div className='flex-1 text-center font-secondary lg:text-left'>
-            <h1 className='text-[60px] font-bold leading-[0.8] lg:text-[80px]'>
+            <motion.h1
+              variants={fadeIn('up', 0.3)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{
+                once: false, amount
+                  : 0.7
+              }}
+              className='text-[65px] font-bold leading-[0.8] lg:text-[85px]'>
               Ralph <span>Saladino</span>
-            </h1>
-            <div className='mb-6 text-[30px] lg:text-[40px] font-secondary font-semibold uppercase leading-[1]'>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn('up', 0.3)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{
+                once: false, amount
+                  : 0.7
+              }}
+              className='mb-6 text-[30px] lg:text-[40px] font-secondary font-semibold uppercase leading-[1]'>
               <span className='text-white mr-4'>I am a</span>
               <TypeAnimation
                 sequence={[
@@ -33,18 +49,42 @@ const Banner = () => {
                 wrapper='span'
                 repeat={Infinity}
               />
-            </div>
-            <p className='mb-8 max-w-lg mx-auto lg:mx-0'>
+            </motion.div>
+            <motion.p
+              variants={fadeIn('up', 0.3)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{
+                once: false, amount
+                  : 0.7
+              }}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'>
               Lorem ipsum dolor sit amet. Id sunt laborum sed rerum quia vel voluptatem consequatur ut accusamus cupiditate vel voluptatem possimus.
-            </p>
-            <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            </motion.p>
+            <motion.div
+              variants={fadeIn('up', 0.3)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{
+                once: false, amount
+                  : 0.7
+              }}
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
               <button className='btn btn-lg'>Contact me</button>
               <a href='#' className='text-gradient btn-link'>
-                My Porffolio
+                My Portfolio
               </a>
-            </div>
+            </motion.div>
             { }
-            <div className='flex text-[20px] gap-x-5 max-w-max mx-auto lg:mx-0'>
+            <motion.div
+              variants={fadeIn('up', 0.3)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{
+                once: false, amount
+                  : 0.7
+              }}
+              className='flex text-[20px] gap-x-5 max-w-max mx-auto lg:mx-0'>
               <a href='https://github.com/hplar05'>
                 <FaGithub />
               </a>
@@ -54,17 +94,25 @@ const Banner = () => {
               <a href='#'>
                 <FaYoutube />
               </a>
-            </div>
+            </motion.div>
           </div>
           {/* Modified image */}
-          <div className='mx-auto lg:mr-14'>
+          <motion.div
+            variants={fadeIn('down', 0.3)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{
+              once: false, amount
+                : 0.7
+            }}
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[420px]'>
             <img
               src={Image}
               alt=''
               className='rounded-3xl h-90' // Replace 'h-50' with the desired height class
               width='400px' // Replace '600px' with the desired width value
             />
-          </div>
+          </motion.div>
 
 
 
