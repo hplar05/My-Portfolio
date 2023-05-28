e033790924cbafda589936010c022da3300235f3
Hp Lar <Ralph.saladino05@gmail.com>
2023-05-28 11:58:23 +0800

    commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,17 +7,61 @@ import { fadeIn } from '../variants';
 
 const Banner = () => {
   return (
-    <section className='section' id='home'>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
       <div className="container mx-auto">
-        <div>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
           {/* Your text */}
-          <div></div>
+          <div className='flex-1 text-center font-secondary lg:text-left'>
+            <h1 className='text-[50px] font-bold leading-[0.8] lg:text-[70px]'>
+              Ralph <span>Saladino</span>
+            </h1>
+            <div className='mb-6 text-[30px] lg:text-[40px] font-secondary font-semibold uppercase leading-[1]'>
+              <span className='text-white mr-4'>I am a</span>
+              <TypeAnimation 
+              sequence={[
+                'Mobile Dev',
+                2000,
+                'Web Dev',
+                2000,
+                'Aspiring Student',
+                2000,
+                'Eating Machine',
+                2000,
+              ]} 
+              speed={40}
+              className='text-accent'
+              wrapper='span'
+              repeat={Infinity}
+              />
+            </div>
+            <p className='mb-8 max-w-lg mx-auto lg:mx-0'>
+              Lorem ipsum dolor sit amet. Id sunt laborum sed rerum quia vel voluptatem consequatur ut accusamus cupiditate vel voluptatem possimus.
+              </p>
+              <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+                <button className='btn btn-lg'>Contact me</button>
+                <a href='#' className='text-gradient btn-link'>
+                  My Porffolio
+                  </a>
+              </div>
+              {}
+              <div className='flex text-[20px] gap-x-5 max-w-max mx-auto lg:mx-0'>
+                <a href='https://github.com/hplar05'>
+                  <FaGithub />
+                </a>
+                <a href='https://www.facebook.com/nasus.main.16'>
+                  <FaFacebook />
+                </a>
+                <a href='#'>
+                  <FaYoutube />
+                </a>
+              </div>
+          </div>
           {/* Modified image */}
-          <div className="">
+          <div className='items-center flex-1 mx-auto'>
             <img
               src={Image}
               alt=''
-              className="w-80 h-80 rounded-full object-cover"
+              className='w-80 h-80 rounded-3xl'
             />
           </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <header className="py-8">
+    <header className="py-2">
       <div className="container mx-auto">
         <div className="flex justify-between items-center ">
           <a href="#">
