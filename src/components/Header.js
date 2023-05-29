0c9db5fa60527eec332d9cdd469cfafbb75ba60a
Hp Lar <Ralph.saladino05@gmail.com>
2023-05-29 23:26:04 +0800

    asdasd

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="py-2">
       <div className="container mx-auto">
-      <div className="flex justify-between items-center min-h-[7vh] lg:min-h-[7vh]">
+      <div className="flex justify-between items-center min-h-[4vh] lg:min-h-[4vh]">
   <a href="#">
     <img src={Logo} alt="Logo" style={{ height: '2rem' }} /> {/* Adjust the height using inline styles */}
   </a>
