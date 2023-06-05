53ed7c7f6a749df32161a601d5b8e1a2b6f73355
Hp Lar <Ralph.saladino05@gmail.com>
2023-06-05 12:47:42 +0800

    commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,11 +6,11 @@ import { BiDetail, BiBriefcase, BsChatSquare, BiPhoneCall } from 'react-icons/bi
 import { Link } from 'react-scroll'
 const Nav = () => {
   return (
-    <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
+    <nav className='fixed bottom-2 lg:bottom-5 w-full overflow-hidden z-50'>
       <div className='container mx-auto'>
 
         <div className='w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full 
-    max-w-[460px] mx-auto px-8 flex justify-between text-2xl text-white/50 items-center'>
+    max-w-[350px] mx-auto px-8 flex justify-between text-2xl text-white/50 items-center'>
           <Link to='home'
             activeClass='active'
             smooth={true}
--- src/components/Work.js
+++ src/components/Work.js
@@ -10,25 +10,25 @@ const Work = () => {
   return <section className='section' id='work'>
     <div className="container mx-auto">
       <div className='flex flex-col lg:flex-row gap-x-10 '>
-        <div 
-            variants={fadeIn('right', 0.5)}
-            initial="hidden"
-            whileInView={'show'}
-            viewport={{
-              once: false, amount
-                : 0.3
-            }}
-        className='flex-1 flex flex-col gap-y-20 mb-10 lg:mb-0'>
+        <div
+          variants={fadeIn('right', 0.5)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{
+            once: false, amount
+              : 0.3
+          }}
+          className='flex-1 flex flex-col gap-y-20 mb-10 lg:mb-0'>
           <div>
             <h2 className='h2 leading-tight text-accent'>
               My Latest <br />
               Work.
             </h2>
-            <p className='max-w-sm mb-16'>
-              I am a passionate web development student, constantly seeking to expand my knowledge in this field. I find the dynamic nature of web development fascinating and enjoy discovering new possibilities.
+            <p className='max-w-sm mb-5'>
+            I have immersed myself in web and mobile app development, working on various projects to enhance my skills and expertise. Here's a brief overview of my notable works in creating web and mobile apps.
             </p>
             <a href="https://github.com/hplar05/">
-            <button className='btn btn-sm'>View all project</button> </a>
+              <button className='btn btn-sm'>View all project</button> </a>
           </div>
 
           <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl cursor-pointer'>
@@ -47,21 +47,21 @@ const Work = () => {
         </div>
 
         <div
-              variants={fadeIn('left', 0.5)}
-              initial="hidden"
-              whileInView={'show'}
-              viewport={{
-                once: false, amount
-                  : 0.3
-              }}
-         className='flex-1 flex flex-col gap-y-5'>
+          variants={fadeIn('left', 0.5)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{
+            once: false, amount
+              : 0.3
+          }}
+          className='flex-1 flex flex-col gap-y-5'>
           <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl cursor-pointer'>
             <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
 
             <img className='group-hover:scale-125 transition-all duration-500' src={Img2} alt="" />
             <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
               <span className=''>
-                Html Vanilla
+                Html, Css, and Javascript Project
               </span>
             </div>
             <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
@@ -76,7 +76,7 @@ const Work = () => {
             <img className='group-hover:scale-125 transition-all duration-500' src={Img3} alt="" />
             <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
               <span className=''>
-                Html Vanilla
+                Html, Css, and Javascript Project
               </span>
             </div>
             <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
