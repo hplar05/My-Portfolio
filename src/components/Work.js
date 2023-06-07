eaf3065da27e01e4eb17f9ef31684a7929a257f5
Hp Lar <Ralph.saladino05@gmail.com>
2023-06-08 05:11:40 +0800

    commit

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,7 +9,7 @@ import Img3 from '../assets/img3.png'
 const Work = () => {
   return <section className='section' id='work'>
     <div className="container mx-auto">
-      <div className='flex flex-col lg:flex-row gap-x-3 '>
+      <div className='flex flex-col lg:flex-row gap-x-5 '>
         <div
           variants={fadeIn('right', 0.5)}
           initial="hidden"
@@ -54,7 +54,7 @@ const Work = () => {
             once: false, amount
               : 0.3
           }}
-          className='flex-1 flex flex-col gap-y-5'>
+          className='flex-1 flex flex-col gap-y-10'>
           <div className='group relative overflow-hidden border-2 border-black/60 rounded-xl cursor-pointer'>
             <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
 
