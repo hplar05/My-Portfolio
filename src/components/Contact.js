330b48f71b6aa00446487d238719ad76668ccc40
Hp Lar <Ralph.saladino05@gmail.com>
2023-06-13 12:54:58 +0800

    add contact us function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { motion } from 'framer-motion'
 import { fadeIn } from '../variants';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_lxj0psa', 'template_iji3ion', form.current, 'dYa18ARENP1rBSqMx')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent");
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (<section className='py-16 lg:selection min-h-[90vh] lg:min-h-[95vh] flex items-center' id='contact'>
     <div className="container mx-auto">
       <div className='flex flex-col lg:flex-row'>
@@ -13,15 +28,15 @@ const Contact = () => {
               Get in touch
             </h4>
             <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>
-              Let's work <br /> together!</h2>
+              Contact Me <br /> anytime!</h2>
           </div>
         </div>
 
-        <form className='text-black flex-1 border border-black rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start'>
-          <input className='bg-trasparent border-b border-black py-3 outline-none w-full placeholder:text-black focus:border-accent transition-all' type="text" placeholder='Your name' required />
-          <input className='bg-trasparent border-b border-black py-3 outline-none w-full placeholder:text-black focus:border-accent transition-all' type="email" placeholder='Your email' required />
-          <textarea className='bg-trasparent border-b border-black py-3 outline-none w-full placeholder:text-black focus:border-accent transition-all resize-none mb-12' placeholder='Your Message' required></textarea>
-          <button className='btn btn-lg'>Send Message</button>
+        <form  ref={form} onSubmit={sendEmail} className='text-black flex-1 border border-black rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start'>
+          <input className='bg-trasparent border-b border-black py-3 outline-none w-full placeholder:text-black focus:border-accent transition-all user_name' name="user_name" type="text" placeholder='Your name' required />
+          <input className='bg-trasparent border-b border-black py-3 outline-none w-full placeholder:text-black focus:border-accent transition-all email' name="user_name" type="email" placeholder='Your email' required />
+          <textarea className='bg-trasparent border-b border-black py-3 outline-none w-full placeholder:text-black focus:border-accent transition-all resize-none mb-12 message' name="message"  placeholder='Your Message' required></textarea>
+          <input className='btn btn-lg text-center' type="submit" value="Send Message"/>
         </form>
       </div>
     </div>
