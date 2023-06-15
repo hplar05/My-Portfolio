2f2a002eec66eaa70756e0892bc373065f94a02b
Hp Lar <Ralph.saladino05@gmail.com>
2023-06-15 13:21:50 +0800

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,19 @@ import Logo from '../assets/logo.png';
 const Header = () => {
   return (
     <header className="py-3">
-      <div className="container lg:mx-4">
-        <div className="flex justify-between items-center min-h-[4vh] lg:min-h-[4vh]">
-          <a href="#">
-            <img src={Logo} alt="Logo" style={{ height: '2rem' }} /> {/* Adjust the height using inline styles */}
-          </a>
-
-        </div>
-
+    <div className="container lg:mx-4">
+      <div className="flex justify-between items-center min-h-[4vh] lg:min-h-[4vh]">
+        <a href="#">
+          <img
+            src={Logo}
+            alt="Logo"
+            style={{ height: '2rem', borderRadius: '10%' }}
+          />
+        </a>
       </div>
-    </header>
+    </div>
+  </header>
+  
   );
 };
 
